17c7bcee9d44f889bd92026f31f18c616da33a0d
Marcus Kofoed Kirkegaard <mkki@itu.dk>
2023-09-05 12:37:57 +0200

    cheep command added, to add new cheep from commandline Co-authored-by: Rasmus <rehe@itu.dk>, Patrick <pahe@itu.dk>, Emil <park@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -10,50 +10,53 @@ class Program
   
   public static void Main(String[] args)
   {
-    
 
     if (args[0] == "read") 
     {
         try
-    {
-        using (var sr = new StreamReader("chirp_cli_db.csv"))
         {
-            sr.ReadLine();
-            
-            string currentLine;
-            while((currentLine = sr.ReadLine()) != null)
+            using (var sr = new StreamReader("chirp_cli_db.csv"))
             {
+                sr.ReadLine();
                 
-                string author = currentLine.Substring(0,currentLine.IndexOf(","));
-                
-                string timeStamp = currentLine.Substring(currentLine.LastIndexOf(",") + 1);
-                long seconds = long.Parse(timeStamp);
-                DateTime convertedTime = new DateTime(1970,1,1,0,0,0,0, DateTimeKind.Utc);
-                convertedTime = convertedTime.AddSeconds(seconds);
-                convertedTime = convertedTime.ToLocalTime();
+                string currentLine;
+                while((currentLine = sr.ReadLine()) != null)
+                {
+                    
+                    string author = currentLine.Substring(0,currentLine.IndexOf(","));
+                    
+                    string timeStamp = currentLine.Substring(currentLine.LastIndexOf(",") + 1);
+                    long seconds = long.Parse(timeStamp);
+                    DateTime convertedTime = new DateTime(1970,1,1,0,0,0,0, DateTimeKind.Utc);
+                    convertedTime = convertedTime.AddSeconds(seconds);
+                    convertedTime = convertedTime.ToLocalTime();
 
-                int lengthOfMessage = currentLine.LastIndexOf(",") - currentLine.IndexOf(",");
-                string message = currentLine.Substring(currentLine.IndexOf(",") +1, lengthOfMessage -1);
+                    int lengthOfMessage = currentLine.LastIndexOf(",") - currentLine.IndexOf(",");
+                    string message = currentLine.Substring(currentLine.IndexOf(",") +1, lengthOfMessage -1);
 
-                // Console.WriteLine(author + " @ " + );
-                string cheep = $"{author} @ {convertedTime} {message}";
-                Console.WriteLine(cheep);
+                    string cheep = $"{author} @ {convertedTime} {message}";
+                    Console.WriteLine(cheep);
+                }   
             }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("This file could not be read");
+            Console.WriteLine(e.Message);
+        }
+    } 
+    else if (args[0] == "cheep")
+    {
+        DateTimeOffset convertedTime = DateTimeOffset.UtcNow;
+        // convertedTime = convertedTime;
+        string author = Environment.UserName;
+        string message = $"\"{args[1]}\"";
+        string cheep = $"{author},{message},{convertedTime.ToUnixTimeSeconds()}";
 
-            
+        using (StreamWriter sw = File.AppendText("chirp_cli_db.csv"))
+        {
+            sw.WriteLine(cheep);
         }
     }
-    catch (IOException e)
-    {
-        Console.WriteLine("This file could not be read");
-        Console.WriteLine(e.Message);
-    }
-    } 
-
-
-
   }
-
 }
-
-

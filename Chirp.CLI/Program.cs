561d191c0741eb23a5fb50ec42e5f34b6dd347e9
Marcus Kofoed Kirkegaard <mkki@itu.dk>
2023-09-05 12:17:44 +0200

    Read from csv with commandline output Co-authored-by: Rasmus <rehe@itu.dk>, Patrick <pahe@itu.dk>, Emil <park@itu.dk>

---
+++ Chirp.CLI/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System; 
+using System.IO;
+
+class Program
+{
+  
+  public static void Main(String[] args)
+  {
+    
+
+    if (args[0] == "read") 
+    {
+        try
+    {
+        using (var sr = new StreamReader("chirp_cli_db.csv"))
+        {
+            sr.ReadLine();
+            
+            string currentLine;
+            while((currentLine = sr.ReadLine()) != null)
+            {
+                
+                string author = currentLine.Substring(0,currentLine.IndexOf(","));
+                
+                string timeStamp = currentLine.Substring(currentLine.LastIndexOf(",") + 1);
+                long seconds = long.Parse(timeStamp);
+                DateTime convertedTime = new DateTime(1970,1,1,0,0,0,0, DateTimeKind.Utc);
+                convertedTime = convertedTime.AddSeconds(seconds);
+                convertedTime = convertedTime.ToLocalTime();
+
+                int lengthOfMessage = currentLine.LastIndexOf(",") - currentLine.IndexOf(",");
+                string message = currentLine.Substring(currentLine.IndexOf(",") +1, lengthOfMessage -1);
+
+                // Console.WriteLine(author + " @ " + );
+                string cheep = $"{author} @ {convertedTime} {message}";
+                Console.WriteLine(cheep);
+            }
+
+            
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("This file could not be read");
+        Console.WriteLine(e.Message);
+    }
+    } 
+
+
+
+  }
+
+}
+
+

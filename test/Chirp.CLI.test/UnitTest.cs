ebf24719b274de475369515f0158be8d0ced1835
Patrick Herlin Henriksen <pahe@itu.dk>
2023-09-26 13:19:18 +0200

    Fixed unittest for github use

--- test/Chirp.CLI.test/UnitTest.cs
+++ test/Chirp.CLI.test/UnitTest.cs
@@ -1,5 +1,5 @@
 namespace Chirp.CLI.test;
-
+using System.Globalization;
 
 
 public class UserInterfaceTest
@@ -7,7 +7,15 @@ public class UserInterfaceTest
     [Fact]
     public void StringFormatting()
     {
+
         var testCheep = new Cheep("Test Bruger", "Dette er en test", 1695121763);
-        Assert.Equal("Test Bruger @ 19-09-23 13:09: Dette er en test", testCheep.FormattedCheep());
+        DateTime convertedTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        convertedTime = convertedTime.AddSeconds(1695121763);
+        convertedTime = convertedTime.ToLocalTime();
+        string formattedTime = convertedTime.ToString("dd-MM-yy HH:mm");
+
+        var testMessage = "Test Bruger @ " + formattedTime + ": Dette er en test";
+
+        Assert.Equal(testMessage, testCheep.FormattedCheep());
     }
 }

5056fed564f8de5ffce7d3ad46ad617db5405be9
Rasmus Emil Sylvest Hendil <rehe@itu.dk>
2023-10-03 12:08:01 +0200

    moving files to uphold proper structure

--- src/Chirp.CLI/UserInterface.cs
+++
@@ -1,8 +0,0 @@
-class UserInterface{
-    public static void printCheeps(IEnumerable<Cheep> cheeps){
-        foreach(Cheep result in cheeps)
-        {
-            Console.WriteLine(result.FormattedCheep());
-        }
-    }
-}

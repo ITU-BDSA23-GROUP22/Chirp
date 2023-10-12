0c69998bb83a5a73957a64cdef9fc3831310508e
Patrick Herlin Henriksen <pahe@itu.dk>
2023-10-12 17:38:05 +0200

    Azure test

--- src/Chirp.Razor/Database/ChirpDBContext.cs
+++ src/Chirp.Razor/Database/ChirpDBContext.cs
@@ -27,7 +27,7 @@ public class ChirpContext : DbContext
     // The following configures EF to create a Sqlite database file in the
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source=home/cheeping.db");
+        => options.UseSqlite($"Data Source=/cheeping.db");
 
 
 }

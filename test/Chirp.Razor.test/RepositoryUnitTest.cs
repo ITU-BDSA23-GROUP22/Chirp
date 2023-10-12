bfa2373a45638489c199a7f01df3a8e80bcab3c1
Emil Parkel <park@itu.dk>
2023-10-12 14:56:11 +0200

    Implemented GetCheepsByAuthor unit test

--- test/Chirp.Razor.test/RepositoryUnitTest.cs
+++ test/Chirp.Razor.test/RepositoryUnitTest.cs
@@ -7,6 +7,13 @@ public class RepositoryUnitTest
     [Fact]
     public void TestNewUser()
     {
+        CheepRepository repo = new CheepRepository();
+
+        string name = "John Doe";
+        string email = "john.doe@example.com";
+        repo.AddAuthor(name, email);
+
+
     }
 
     [Fact]
@@ -22,5 +29,13 @@ public class RepositoryUnitTest
     [Fact]
     public void TestCheepsByAuthor()
     {
+        CheepRepository repo = new CheepRepository();
+
+        string name = "John Doe";
+        string email = "john.doe@example.com";
+        repo.AddAuthor(name, email);
+        Author author = repo.GetAuthor(name);
+        repo.WriteCheep("hello hello", DateTime.Now, author);
+        Assert.Single(repo.GetCheepsByAuthor(author));
     }
 }

f6f53a9e1b3a5808d390c3350ed671c799589912
Emil Parkel <park@itu.dk>
2023-10-12 15:04:45 +0200

    Implemented DeleteAuthor and WriteCheep unit tests

--- test/Chirp.Razor.test/RepositoryUnitTest.cs
+++ test/Chirp.Razor.test/RepositoryUnitTest.cs
@@ -12,18 +12,37 @@ public class RepositoryUnitTest
         string name = "John Doe";
         string email = "john.doe@example.com";
         repo.AddAuthor(name, email);
-
-
     }
 
     [Fact]
     public void TestDeleteUser()
     {
+        CheepRepository repo = new CheepRepository();
+
+        string name = "John Doe";
+        string email = "john.doe@example.com";
+        repo.AddAuthor(name, email);
+
+        Assert.NotNull(repo.GetAuthor(name));
+
+        Author author = repo.GetAuthor(name);
+        repo.DeleteAuthor(author);
+
+        Assert.Null(repo.GetAuthor(name));
     }
 
     [Fact]
     public void TestNewCheep()
     {
+        CheepRepository repo = new CheepRepository();
+
+        string name = "John Doe";
+        string email = "john.doe@example.com";
+        repo.AddAuthor(name, email);
+
+        Author author = repo.GetAuthor(name);
+        repo.WriteCheep("hello hello", DateTime.Now, author);
+        Assert.Single(repo.GetAllCheeps());
     }
 
     [Fact]

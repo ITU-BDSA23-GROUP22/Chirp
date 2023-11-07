b1143f4fa36afc8c8058d5c31ba1c0a42a6b728c
Marcus Kofoed Kirkegaard <mkki@itu.dk>
2023-11-07 12:31:31 +0100

    Profile page with claims added

---
+++ src/Chirp.Web/Pages/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

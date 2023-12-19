831196dbccc94a5d91e698b15522390c56f15f6b
Marcus Kofoed Kirkegaard <mkki@itu.dk>
2023-12-19 16:14:04 +0100

    Add seedDatabaseConfiguration, configured in appsettings

--- src/Chirp.Web/DatabaseProviderConfig.cs
+++ src/Chirp.Web/DatabaseProviderConfig.cs
@@ -9,6 +9,13 @@ namespace Chirp.Web
 	public class DatabaseProviderConfig
 	{
 		public DatabaseProviderType DatabaseProviderType { get; set; }
+
+		/// <summary>
+		/// 	If true, the database is dropped before updating and seeding.
+		/// </summary>
+		public bool RecreateDatabaseOnStartup { get; set; }
+
+		public bool SeedDatabase { get; set; }
 	}
 }
 
